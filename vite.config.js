ee437a5cf67de4f9ed74d9a1bfea48b3034a30ae
rishi200201 <rishi20020107@gmail.com>
2026-03-12 16:44:30 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/OrderNow/",
+  base: process.env.VITE_BASE_URL || "/sri_saravana_store",
 });
